3b22858fe7dd0ef56d433c4a76123c8aa139f262
Flaykz <david.brouste.perso@gmail.com>
2019-05-03 09:51:29 +1100

    fix

--- config.js
+++ config.js
@@ -1,22 +1,22 @@
 var config = {
     production: {
-        "PORT": 8087,
-        "title": "app",
-        "colour": ['#ffff00', '#00ff00', '#00ffff', '#0000ff'],
+        "PORT": 8086,
+        "title": "CountScore",
+        "colour": ['#ffff00', '#00ff00', '#00ffff', '#0000ff', '#ffff00', '#00ff00', '#00ffff', '#0000ff', '#ffff00'],
         "item": ['', ''],
-        "name": ['Joueur_1', 'Joueur_2', 'Joueur_3', 'Joueur_4']
+        "name": ['Joueur_1', 'Joueur_2', 'Joueur_3', 'Joueur_4', 'Joueur_5', 'Joueur_6', 'Joueur_7', 'Joueur_8', 'Joueur_9']
     },
     productionv2: {
-        "PORT": 8088,
-        "title": "CricketV2",
-        "colour": ['#ffff00', '#00ff00', '#00ffff', '#0000ff'],
+        "PORT": 8086,
+        "title": "CountScore",
+        "colour": ['#ffff00', '#00ff00', '#00ffff', '#0000ff', '#ffff00', '#00ff00', '#00ffff', '#0000ff', '#ffff00'],
         "item": ['', '='],
-        "name": ['Joueur_1', 'Joueur_2', 'Joueur_3', 'Joueur_4']
+        "name": ['Joueur_1', 'Joueur_2', 'Joueur_3', 'Joueur_4', 'Joueur_5', 'Joueur_6', 'Joueur_7', 'Joueur_8', 'Joueur_9']
     },
     default: {
-        "PORT": 8080,
-        "title": "dev",
-        "colour": ['#ffff00', '#00ff00', '#00ffff', '#0000ff'],
+        "PORT": 8086,
+        "title": "CountScoreDev",
+        "colour": ['#ffff00', '#00ff00', '#00ffff', '#0000ff', '#ffff00', '#00ff00', '#00ffff', '#0000ff', '#ffff00'],
         "item": ['', ''],
         "name": ['Joueur_1', 'Joueur_2', 'Joueur_3', 'Joueur_4', 'Joueur_5', 'Joueur_6', 'Joueur_7', 'Joueur_8', 'Joueur_9']
     }
